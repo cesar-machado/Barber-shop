d64f8b4cbc1b52cf8afdce9409921bbdb04c19a3
César Machado <cesarmach19@gmail.com>
2021-07-03 19:46:56 -0300

    Atualizando o projeto

--- App.js
+++ App.js
@@ -3,31 +3,23 @@ import * as Font from "expo-font";
 import { Text, View } from "react-native";
 import { AppLoading } from "expo";
 
-
 import Routes from "./src/routes";
- 
-
 
 const getFonts = () => {
   return Font.loadAsync({
-    'roboto': require("./assets/fonts/Roboto-Regular.ttf"),
+    roboto: require("./assets/fonts/Roboto-Regular.ttf"),
     "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
 };
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false)
+  // const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if(fontsLoaded) {
-    return (
-      <Routes />
-      )
-  } else {
-    return (
-    <AppLoading 
-      startAsync={getFonts}
-      onFinish={() => setFontsLoaded(true)}
-    />
-  
-    )}
+  // if (!fontsLoaded) {
+  //   return (
+  //     <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+  //   );
+  // }
+
+  return <Routes />;
 }
